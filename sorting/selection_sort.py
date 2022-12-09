cd15dcec326f20a625ff41a3c6735aa2f7af804d
Felipe <superfelipex@gmail.com>
2022-12-08 20:40:47 -0400

    v0.2

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def sort(array):
     for index in range(0, len(array)):
@@ -13,5 +14,23 @@ def sort(array):
 vetor = list(range(0, 9999))
 random.shuffle(vetor)
 
-sort(vetor)
 print(vetor)
+
+antes1 = time.time()
+sort(vetor)
+depois1 = time.time()
+total1 = (depois1 - antes1)*1000
+random.shuffle(vetor)
+antes2 = time.time()
+sort(vetor)
+depois2 = time.time()
+total2 = (depois2 - antes2)*1000
+random.shuffle(vetor)
+antes3 = time.time()
+sort(vetor)
+depois3 = time.time()
+total3 = (depois3 - antes3)*1000
+
+totalf = (total1 + total2 + total3 / 3)
+
+print ("A ordenação demorou %0.2f ms" % totalf)
