7a98f983e3d4209ea15dbb0b04632a03b4265dc0
Felipe <superfelipex@gmail.com>
2022-12-09 17:04:22 -0400

    Atualizado para o padrão

--- Quick_sort.py
+++ Quick_sort.py
@@ -2,7 +2,7 @@ import random
 import time
 
 
-def quickSort(alist):
+def sort(alist):
    quickSortHelper(alist,0,len(alist)-1)
 
 def quickSortHelper(alist,first,last):
@@ -45,11 +45,36 @@ def partition(alist,first,last):
 
 vetor = list(range(0, 10000))
 random.shuffle(vetor)
-antes = time.time()
-quickSort(vetor)
-depois = time.time()
-total = (depois - antes)*1000
 
-print(vetor)
+#print(vetor)
 
-print("O tempo total foi: %02f ms" %total)
+print("Realizando ordenações...")
+print("")
+antes1 = time.time()
+sort(vetor)
+depois1 = time.time()
+total1 = (depois1 - antes1)*1000
+print("1... %0.2f ms" % total1)
+
+random.shuffle(vetor)
+antes2 = time.time()
+sort(vetor)
+depois2 = time.time()
+total2 = (depois2 - antes2)*1000
+print("2... %0.2f ms" % total2)
+
+random.shuffle(vetor)
+antes3 = time.time()
+sort(vetor)
+depois3 = time.time()
+total3 = (depois3 - antes3)*1000
+print("3... %0.2f ms" % total3)
+
+totalf = ((total1 + total2 + total3) / 3)
+
+print("")
+print ("Ta... Da!")
+time.sleep(1)
+print ("A ordenação demorou %0.2f ms em média de três execuções." % totalf)
+
+input("Pressione ENTER para finalizar.")
